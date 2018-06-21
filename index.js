0eaa510e82587d4bdf1e401e1200b921331ae066
Colin Wood <colinjameswood@gmail.com>
2018-06-21 10:08:12 -0500

    Adds comments for every airbnb-base rule we override, and tweaks a couple of rules

--- index.js
+++ index.js
@@ -4,23 +4,82 @@ module.exports = {
   ],
   parser: 'babel-eslint',
   rules: {
-    'max-len': 0,
-    'no-console': 0,
-    'padded-blocks': 0,
-    'no-underscore-dangle': 0,
+    // disable max line length
+    'max-len': 'off',
+
+    // allow console.logs, base airbnb warns
+    'no-console': 'off',
+
+    // allow padded blocks
+    'padded-blocks': 'off',
+
+    // dissallow semi colons
     'semi': ['error', 'never'],
+
+    // allow variables that begin with _
+    'no-underscore-dangle': 'off',
+
+    // auto-add semi colons in front of lines that could cause problems (e.g.
+    //  open parens)
     'semi-style': ['error', 'first'],
-    'newline-per-chained-call': 'off',
+
+    // this is actually redundant, already in base airbnb... will leave in case
+    //  they change it
     'no-mixed-spaces-and-tabs': 'error',
+
+    // always requires parens around arrow function params
+    'arrow-parens': ['error', 'always'],
+
+    // always requires arrow function bodies {}
     'arrow-body-style': ['error', 'always'],
-    'indent': ['error', 2, { 'SwitchCase': 1 }],
+
+    // this could be removed I suppose. it allows you to write shorter while()
+    //  loops and such, which never really happens much anyway
     'no-cond-assign': ['error', 'except-parens'],
+
+    // this is actually redundant, already in base airbnb... will leave in case
+    //  they change it
     'comma-dangle': ['error', 'always-multiline'],
-    'no-unused-vars': ['error', { 'args': 'none' } ],
+
+    // allows unused vars in arguments, (e.g.
+    //  `new Promise((resolve, reject) => {} )` but you never use the reject)
+    //
+    // this is useful because it lets you know that an argument is available and
+    //  what it is even if you're not using it right now
+    'no-unused-vars': ['error', { 'args': 'none' }],
+
+    // this allows you to overwrite values on an object passed to a function as
+    //  a parameter
+    //
+    // airbnb has this disabled except for certain variable names ($scope,
+    //  request, e, ...)
     'no-param-reassign': ['error', { 'props': false }],
-    'function-paren-newline': [ 'error', 'consistent' ],
+
+    // they actually changed the base airbnb to be this exact rule, but they
+    //  haven't published it to npm yet?
+    'function-paren-newline': ['error', 'consistent'],
+
+    // this require you to write multiline comments with // like these comments
+    'multiline-comment-style': ['error', 'separate-lines'],
+
+    // allow i++ in for loops, base airbnb has this error for every use of ++
     'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
-    "prefer-destructuring": ["error", { array: false, object: true }],
-    'object-curly-newline': ['error', { ObjectExpression: { multiline: false, consistent: true } }],
-  }
+
+    // do not force array destructuring so you don't have to do
+    //  const [yeah] = response.result
+    //
+    //  instead of
+    //
+    //  const yeah = response.result[0]
+    //
+    // this also becomes a pain in the ass when you want the value at index 1
+    //  instead of 0
+    'prefer-destructuring': ['error', { array: false, object: true }],
+
+    // I think this basically allows you to write a single-line object with more
+    //  than 4 keys. airbnb default is this, but forces you to line break if
+    //  there are more than 4 keys
+    'object-curly-newline': ['error', { ObjectExpression: { multiline: true, consistent: true } }],
+
+  },
 }
